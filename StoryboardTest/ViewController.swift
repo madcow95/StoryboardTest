29b819d33c912daddddcf0758ad9653c833c0b8d
madcow95 <lobasketve@naver.com>
2023-02-09 22:38:00 +0900

    Initial Commit

---
+++ StoryboardTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardTest
+//
+//  Created by ChoiKwangWoo on 2023/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
