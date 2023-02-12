589ec0afbc314dfff9b2c55830f01df47c570dfc
madcow95 <lobasketve@naver.com>
2023-02-12 19:05:35 +0900

    first app

--- StoryboardTest/ViewController.swift
+++ StoryboardTest/ViewController.swift
@@ -3,17 +3,33 @@
 //  StoryboardTest
 //
 //  Created by ChoiKwangWoo on 2023/02/09.
+//  버튼 누르면 이미지 바뀌는 간단한 앱
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let symbols: [String] = ["moon", "cloud", "wind", "snowflake", "tornado", "hurricane"]
+    
+    func reload() {
+        let symbol = symbols.randomElement()!
+        imageView.image = UIImage(systemName: symbol)
+        label.text = symbol
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        reload()
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        reload()
+    }
+    
 }
 
